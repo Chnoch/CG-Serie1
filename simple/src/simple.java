3bca0cfe964e3cd613c310480a8cb5af6ce7e424
Chnoch <bas@students.unibe.ch>
2010-10-06 11:55:59 +0200

    further expansion for cylinder. crooked things not working though...

--- simple/src/simple.java
+++ simple/src/simple.java
@@ -7,183 +7,222 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * Implements a simple application that opens a 3D rendering window and 
- * shows a rotating cube.
+ * Implements a simple application that opens a 3D rendering window and shows a
+ * rotating cube.
  */
-public class simple
-{	
-	static RenderPanel renderPanel;
-	static RenderContext renderContext;
-	static SimpleSceneManager sceneManager;
-	static Shape shape;
-	static float angle;
+public class simple {
+    static RenderPanel renderPanel;
+    static RenderContext renderContext;
+    static SimpleSceneManager sceneManager;
+    static Shape shape;
+    static float angle;
 
-	/**
-	 * An extension of {@link GLRenderPanel} or {@link SWRenderPanel} to 
-	 * provide a call-back function for initialization. 
-	 */ 
-	public final static class SimpleRenderPanel extends GLRenderPanel
-	{
-		/**
-		 * Initialization call-back. We initialize our renderer here.
-		 * 
-		 * @param r	the render context that is associated with this render panel
-		 */
-		public void init(RenderContext r)
-		{
-			renderContext = r;
-			renderContext.setSceneManager(sceneManager);
-	
-			// Register a timer task
-		    Timer timer = new Timer();
-		    angle = 0.01f;
-//		    timer.scheduleAtFixedRate(new AnimationTask(), 0, 10);
-		}
-	}
-/*
-	/**
-	 * A timer task that generates an animation. This task triggers
-	 * the redrawing of the 3D scene every time it is executed.
-	 */
-	public static class AnimationTask extends TimerTask
-	{
-		public void run()
-		{
-			// Update transformation
-    		Matrix4f t = shape.getTransformation();
-    		Matrix4f rotX = new Matrix4f();
-    		rotX.rotX(angle);
-    		Matrix4f rotY = new Matrix4f();
-    		rotY.rotY(angle);
-    		t.mul(rotX);
-    		t.mul(rotY);
-    		shape.setTransformation(t);
-    		
-    		// Trigger redrawing of the render window
-    		renderPanel.getCanvas().repaint(); 
-		}
-	}
+    /**
+     * An extension of {@link GLRenderPanel} or {@link SWRenderPanel} to provide
+     * a call-back function for initialization.
+     */
+    public final static class SimpleRenderPanel extends GLRenderPanel {
+        /**
+         * Initialization call-back. We initialize our renderer here.
+         * @param r
+         *            the render context that is associated with this render
+         *            panel
+         */
+        public void init(RenderContext r) {
+            renderContext = r;
+            renderContext.setSceneManager(sceneManager);
 
-	/**
-	 * A mouse listener for the main window of this application. This can be
-	 * used to process mouse events.
-	 */
-	public static class SimpleMouseListener implements MouseListener
-	{
-    	public void mousePressed(MouseEvent e) {}
-    	public void mouseReleased(MouseEvent e) {}
-    	public void mouseEntered(MouseEvent e) {}
-    	public void mouseExited(MouseEvent e) {}
-    	public void mouseClicked(MouseEvent e) {}
-	}
-	
-	
-	
-	
-	
-	private static float x[], c[];
-	private static int indices[];
-	
-	public static void makeCylinder(int resolution) {
-	    int angle = 360 / resolution;
-	    x = new float[9*resolution];
-	    x[0]=0;
-	    x[1]=0;
-	    x[2]=0;
-	    int a=2;
-	    for (int i=0; i<3*resolution-3; i++) {
-	        x[++a]= (float) Math.cos(i*angle);
-	        x[++a]= (float) Math.sin(i*angle);
-            x[++a]=0;
-	    }
-	    
-	    c = new float[9*resolution];
-	    a=-1;
-	    for (int i=0;i<resolution;i++) {
-	        c[++a]=1;
-	        c[++a]=1;
-	        c[++a]=1;
-	        
-	        c[++a]=1;
-	        c[++a]=1;
-	        c[++a]=1;
-	        
-	        c[++a]=1;
-	        c[++a]=1;
-	        c[++a]=1;
-	    }
-	    
-	    indices = new int[3*resolution];
-	    a=-1;
-	    for (int i=0;i<resolution;i++){
-	        indices[++a]=0;
-	        indices[++a]=(i+1);
-	        indices[++a]=(i+2);
-	    }
-	    //correction to combine with first index;
-	    indices[a]=1;
-	}
-	
-	/**
-	 * The main function opens a 3D rendering window, constructs a simple 3D
-	 * scene, and starts a timer task to generate an animation.
-	 */
-	public static void main(String[] args)
-	{		
-		// Make a simple geometric object: a cube
-		
-		// The vertex positions of the cube
-		float v[] = {-1,-1,1, 1,-1,1, 1,1,1, -1,1,1,		// front face
-			         -1,-1,-1, -1,-1,1, -1,1,1, -1,1,-1,	// left face
-				  	 1,-1,-1,-1,-1,-1, -1,1,-1, 1,1,-1,		// back face
-					 1,-1,1, 1,-1,-1, 1,1,-1, 1,1,1,		// right face
-					 1,1,1, 1,1,-1, -1,1,-1, -1,1,1,		// top face
-					-1,-1,1, -1,-1,-1, 1,-1,-1, 1,-1,1};	// bottom face
-		
-		// The vertex colors
-		/*float c[] = {1,0,0, 1,0,0, 1,0,0, 1,0,0,
-				     0,1,0, 0,1,0, 0,1,0, 0,1,0,
-					 1,0,0, 1,0,0, 1,0,0, 1,0,0,
-					 0,1,0, 0,1,0, 0,1,0, 0,1,0,
-					 0,0,1, 0,0,1, 0,0,1, 0,0,1,
-					 0,0,1, 0,0,1, 0,0,1, 0,0,1};
-*/
-		makeCylinder(6);
-		// Construct a data structure that stores the vertices, their
-		// attributes, and the triangle mesh connectivity
-		VertexData vertexData = new VertexData(x.length);
-		vertexData.addElement(c, VertexData.Semantic.COLOR, 3);
-		vertexData.addElement(x, VertexData.Semantic.POSITION, 3);
-		
-		// The triangles (three vertex indices for each triangle)
-		/*int indices[] = {0,2,3, 0,1,2,			// front face
-						 4,6,7, 4,5,6,			// left face
-						 8,10,11, 8,9,10,		// back face
-						 12,14,15, 12,13,14,	// right face
-						 16,18,19, 16,17,18,	// top face
-						 20,22,23, 20,21,22};	// bottom face
-*/
-		vertexData.addIndices(indices);
-				
-		// Make a scene manager and add the object
-		sceneManager = new SimpleSceneManager();
-		shape = new Shape(vertexData);
-		sceneManager.addShape(shape);
+            // Register a timer task
+            Timer timer = new Timer();
+            angle = 0.01f;
+            // timer.scheduleAtFixedRate(new AnimationTask(), 0, 10);
+        }
+    }
 
-		// Make a render panel. The init function of the renderPanel
-		// (see above) will be called back for initialization.
-		renderPanel = new SimpleRenderPanel();
-		
-		// Make the main window of this application and add the renderer to it
-		JFrame jframe = new JFrame("Cylinder");
-		jframe.setSize(500, 500);
-		jframe.setLocationRelativeTo(null); // center of screen
-		jframe.getContentPane().add(renderPanel.getCanvas());// put the canvas into a JFrame window
+    /*
+    	/**
+    	 * A timer task that generates an animation. This task triggers
+    	 * the redrawing of the 3D scene every time it is executed.
+    	 */
+    public static class AnimationTask extends TimerTask {
+        public void run() {
+            // Update transformation
+            Matrix4f t = shape.getTransformation();
+            Matrix4f rotX = new Matrix4f();
+            rotX.rotX(angle);
+            Matrix4f rotY = new Matrix4f();
+            rotY.rotY(angle);
+            t.mul(rotX);
+            t.mul(rotY);
+            shape.setTransformation(t);
 
-		// Add a mouse listener
-	    jframe.addMouseListener(new SimpleMouseListener());
-		   	    	    
-	    jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    jframe.setVisible(true); // show window
-	}
+            // Trigger redrawing of the render window
+            renderPanel.getCanvas().repaint();
+        }
+    }
+
+    /**
+     * A mouse listener for the main window of this application. This can be
+     * used to process mouse events.
+     */
+    public static class SimpleMouseListener implements MouseListener {
+        public void mousePressed(MouseEvent e) {
+        }
+
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        public void mouseExited(MouseEvent e) {
+        }
+
+        public void mouseClicked(MouseEvent e) {
+        }
+    }
+
+    private static float x[], c[];
+    private static int indices[];
+
+    public static void makeCylinder(int resolution) {
+        int angle = 360 / resolution;
+        x = new float[2* 9 * resolution];
+        x[0] = 0;
+        x[1] = 0;
+        x[2] = 1;
+        int a = 2;
+        for (int i = 0; i < 3 * resolution - 3; i++) {
+            x[++a] = (float) Math.cos(i * angle);
+            x[++a] = (float) Math.sin(i * angle);
+            x[++a] = 1;
+        }
+        
+        x[++a]=0;
+        x[++a]=0;
+        x[++a]=-1;
+        
+        for (int i = 0; i < 3 * resolution - 3; i++) {
+            x[++a] = (float) Math.cos(i * angle);
+            x[++a] = (float) Math.sin(i * angle);
+            x[++a] = -1;
+        }
+
+        c = new float[2* 9 * resolution];
+        a = -1;
+        for (int i = 0; i < resolution; i++) {
+            c[++a] = 1;
+            c[++a] = 1;
+            c[++a] = 1;
+
+            c[++a] = 0;
+            c[++a] = 0;
+            c[++a] = 0;
+        }
+
+        //2*3*resolution (top and bottom) + 6*resolution (sides)
+        indices = new int[12*resolution];
+        a = -1;
+        // top
+        for (int i = 0; i < resolution; i++) {
+            indices[++a] = 0;
+            indices[++a] = (i + 1);
+            indices[++a] = (i + 2);
+        }
+        
+        indices[a]=1;
+        
+        // bottom
+        for (int i = resolution+1; i < 2*resolution+1; i++) {
+            indices[++a] = resolution+1;
+            indices[++a] = (i + 1);
+            indices[++a] = (i + 2);
+        }
+        indices[a]=resolution+2;
+        
+        for (int i = 0; i < resolution-1;i++) {
+            indices[++a] = i+1;
+            indices[++a] = resolution+ i+1 +1;
+            indices[++a] = resolution+ i+1 +2;
+            
+            indices[++a] = i+2;
+            indices[++a] = resolution+ i+1 +1;
+            indices[++a] = resolution+ i+1 +2;
+            
+        }
+        indices[++a] = resolution;
+        indices[++a] = 2*resolution+1;
+        indices[++a] = resolution+2;
+        
+        indices[++a] = 1;
+        indices[++a] = 2*resolution+1;
+        indices[++a] = resolution+2;
+        
+        
+        // correction to combine with first index;
+    }
+
+    /**
+     * The main function opens a 3D rendering window, constructs a simple 3D
+     * scene, and starts a timer task to generate an animation.
+     */
+    public static void main(String[] args) {
+        // Make a simple geometric object: a cube
+
+        // The vertex positions of the cube
+        float v[] = { -1,-1,1, 1,-1,1, 1,1,1, -1,1,1, // front face
+                -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, 1, -1, // left face
+                1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, // back face
+                1, -1, 1, 1, -1, -1, 1, 1, -1, 1, 1, 1, // right face
+                1, 1, 1, 1, 1, -1, -1, 1, -1, -1, 1, 1, // top face
+                -1, -1, 1, -1, -1, -1, 1, -1, -1, 1, -1, 1 }; // bottom face
+
+        // The vertex colors
+        /*float c[] = {1,0,0, 1,0,0, 1,0,0, 1,0,0,
+        		     0,1,0, 0,1,0, 0,1,0, 0,1,0,
+        			 1,0,0, 1,0,0, 1,0,0, 1,0,0,
+        			 0,1,0, 0,1,0, 0,1,0, 0,1,0,
+        			 0,0,1, 0,0,1, 0,0,1, 0,0,1,
+        			 0,0,1, 0,0,1, 0,0,1, 0,0,1};
+        */
+        makeCylinder(4);
+        // Construct a data structure that stores the vertices, their
+        // attributes, and the triangle mesh connectivity
+        VertexData vertexData = new VertexData(x.length);
+        vertexData.addElement(x, VertexData.Semantic.POSITION, 3);
+        vertexData.addElement(c, VertexData.Semantic.COLOR, 3);
+
+        // The triangles (three vertex indices for each triangle)
+        /*int indices[] = {0,2,3, 0,1,2,			// front face
+        				 4,6,7, 4,5,6,			// left face
+        				 8,10,11, 8,9,10,		// back face
+        				 12,14,15, 12,13,14,	// right face
+        				 16,18,19, 16,17,18,	// top face
+        				 20,22,23, 20,21,22};	// bottom face
+        */
+        vertexData.addIndices(indices);
+
+        // Make a scene manager and add the object
+        sceneManager = new SimpleSceneManager();
+        shape = new Shape(vertexData);
+        sceneManager.addShape(shape);
+
+        // Make a render panel. The init function of the renderPanel
+        // (see above) will be called back for initialization.
+        renderPanel = new SimpleRenderPanel();
+
+        // Make the main window of this application and add the renderer to it
+        JFrame jframe = new JFrame("Cylinder");
+        jframe.setSize(500, 500);
+        jframe.setLocationRelativeTo(null); // center of screen
+        jframe.getContentPane().add(renderPanel.getCanvas());// put the canvas
+                                                             // into a JFrame
+                                                             // window
+
+        // Add a mouse listener
+        jframe.addMouseListener(new SimpleMouseListener());
+
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setVisible(true); // show window
+    }
 }
